6e6857c7d8c438a5978cc2f93efd7eb685b70c7c
zzpapy <gregory.pace@hotmail.fr>
2023-08-16 15:59:04 +0200

    toto

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,20 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        $list = [];
+        $dir = '../../LaRocca/public/images';
+        $files1 = scandir($dir);
+        $length = count($files1);
+        for ($i = 2; $i <= $length - 1; $i++) {
+            array_push($list, $files1[$i]);
+        }
+        
+     
+        
+
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'list' => $list,
         ]);
     }
 }
